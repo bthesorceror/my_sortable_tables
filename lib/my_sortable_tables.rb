42f1b0fa946fd374d9a47d4b1834eb1e219db121
Brandon Farmer <thepooreskimojoe@gmail.com>
2012-01-22 19:49:54 -0600

    Adding controller module

--- lib/my_sortable_tables.rb
+++ lib/my_sortable_tables.rb
@@ -1,5 +1,32 @@
 require "my_sortable_tables/version"
 
 module MySortableTables
-  # Your code goes here...
+  
+  module SortableController
+
+    def self.included(base)
+      base.send(:include, InstanceMethods)
+      base.class_eval do
+        helper_method :sort_column, :sort_direction
+      end
+    end
+
+    module InstanceMethods
+      
+      def self.sortable_columns(*columns)
+        @@sortable_columns = columns
+      end
+  
+      def sort_column
+        @@sortable_columns.include?(params[:sort]) ? params[:sort] : @@sortable_columns.first if @@sortable_columns
+      end
+
+      def sort_direction
+        %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
+      end
+
+    end
+
+  end
+
 end
