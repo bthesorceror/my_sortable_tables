3ea3314bb2091826116c12186ef156bc43a7292b
Brandon Farmer <thepooreskimojoe@gmail.com>
2012-01-22 20:05:50 -0600

    fixing class methods

--- lib/my_sortable_tables.rb
+++ lib/my_sortable_tables.rb
@@ -1,22 +1,24 @@
 require "my_sortable_tables/version"
 
 module MySortableTables
-  
+
   module SortableController
 
     def self.included(base)
       base.send(:include, InstanceMethods)
+      base.extend(ClassMethods)
       base.class_eval do
         helper_method :sort_column, :sort_direction
       end
     end
 
-    module InstanceMethods
-      
-      def self.sortable_columns(*columns)
+    module ClassMethods
+      def sortable_columns(*columns)
         @@sortable_columns = columns
       end
-  
+    end
+
+    module InstanceMethods
       def sort_column
         @@sortable_columns.include?(params[:sort]) ? params[:sort] : @@sortable_columns.first if @@sortable_columns
       end
@@ -24,7 +26,6 @@ module MySortableTables
       def sort_direction
         %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
       end
-
     end
 
   end
